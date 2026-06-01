3d6c38eebf3999a3da1f7367da16c61112d2766f
Hanganu Augustin <augustin.hanganu@student.usv.ro>
2026-06-01 15:53:20 +0300

    Main.cpp Main pentru rulare in consola

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Intersectie.h"
+using namespace std;
+
+/*int main() {
+    cout << "================================" << endl;
+    cout << "  SIMULATOR TRAFIC RUTIER C++  " << endl;
+    cout << "================================" << endl;
+    cout << "Apasa ENTER pentru a incepe...";
+    cin.get();
+
+    Intersectie intersectie("Piata Unirii");
+    intersectie.ruleaza(20); // ruleaza 20 cicluri
+
+    return 0;
+}
+*/
